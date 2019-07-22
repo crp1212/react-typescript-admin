eb016b05014f58f42ce898f02d78f0be1d1df800
crp1212 <403454318@qq.com>
2019-07-22 11:05:16 +0800

    feat: 添加错误请求默认提示

---
+++ src/apis/index.ts
@@ -0,0 +1,63 @@
+import axios from 'axios'
+import qs from 'qs'
+import { errorNotify } from '@/utils/index'
+function formatParams (method: string, params: any, config?: any) { // 获取正确的参数格式
+  let result
+  if (['get', 'delete'].indexOf(method.toLowerCase()) > -1) {
+    /* params.fromUrl = window.location.href */
+    result = {
+      params: {
+        ...params,
+        ...config
+      }
+    }
+  } else {
+    result = qs.stringify(params)
+  }
+  return result
+}
+function getAxiosFn (methods: string) {
+  let fn: (url: string, params: any, config?: any) => any = () => {}
+  switch (methods) {
+    case 'get':
+      fn = axios.get
+      break;
+    case 'post':
+      fn = axios.post
+    break;
+    case 'delete':
+      fn = axios.delete
+      break;
+    case 'put':
+      fn = axios.put
+    break;
+    default:
+      break;
+  }
+  return fn
+}
+function requestSuccessHandle (res: any, resolve: any, reject: any) { // 成功请求的
+  let data = res.data // 返回的数据
+  resolve(data)
+}
+function requestFailHandle (res: any, resolve: any, reject: any, closeGlobalErrorTips: boolean) {
+  let data = res.data // 如果的是由返回值的
+  let message: string = typeof data === 'string' ? data : data.message
+  reject(message)
+  if (!closeGlobalErrorTips) { // 没有要求关闭全局的错误提示
+    errorNotify(message)
+  }
+}
+
+export const fetch = (method: string, url: string, closeGlobalErrorTips = false) => {
+  return (params = {}, config = {}) => new Promise((resolve, reject) => {
+    let parameter = formatParams(method, params, config)
+    let requesrFn: (url: string, params: any, config?: any) => any  = getAxiosFn(method)
+    requesrFn(url, params, config).then((response: any) => {
+      let status: number = response.status
+      requestSuccessHandle(response, resolve, reject)
+    }).catch((error: any) => {
+      requestFailHandle(error.response, resolve, reject, closeGlobalErrorTips)
+    })
+  })
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,4 @@
-export const a = 1
+import { message } from 'antd'
+export const errorNotify = (str: string) => { // 消息错误提醒
+  message.error(str)
+}
