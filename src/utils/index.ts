18e430480b7454f9e64624be32b7bfd98af3847b
crp1212 <403454318@qq.com>
2019-08-08 23:15:31 +0800

    feat: 新增hexColorToRGB方法

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,15 @@ import { message } from 'antd'
 export const errorNotify = (str: string) => { // 消息错误提醒
   message.error(str)
 }
+export function hexColorToRGB (hexColor = '') { // 
+  if (hexColor.length !== 6) { 
+    console.error('非正确的十六进制颜色') 
+    return []
+  }
+  let tem = []
+  let i = 0
+  while (i < 6) {
+    tem.push(hexColor.slice(i, i += 2))
+  }
+  return tem.map((val: string) => parseInt(val, 16))
+}
