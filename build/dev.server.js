7b0a21c0199564cbac356419a33bdda485ff1903
crp1212 <403454318@qq.com>
2019-08-21 11:46:26 +0800

    feat: 增加eslint ignore配置

--- build/dev.server.js
+++ build/dev.server.js
@@ -7,19 +7,19 @@ const opn =require('opn')
 const webpackConfig = require('./webpack.dev.config.js')
 console.log(config.dev.proxy)
 const options = {
-    hot: true,
-    host: 'localhost',
-    proxy: config.dev.proxy, 
-    contentBase: config.dev.static,
-    historyApiFallback: {
-        index: 'src/index.html'
-    },
+  hot: true,
+  host: 'localhost',
+  proxy: config.dev.proxy, 
+  contentBase: config.dev.static,
+  historyApiFallback: {
+    index: 'src/index.html'
+  },
 }
 webpackDevServer.addDevServerEntrypoints(webpackConfig, options)
 const compiler = webpack(webpackConfig)
 const server = new webpackDevServer(compiler, options)
 
 server.listen(config.dev.port, 'localhost', () => {
-    console.log('dev server listening on port ' + config.dev.port)
-    // opn('http://localhost:' + config.dev.port, { app: 'chrome' })
+  console.log('dev server listening on port ' + config.dev.port)
+  // opn('http://localhost:' + config.dev.port, { app: 'chrome' })
 })
