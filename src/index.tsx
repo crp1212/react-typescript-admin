a51f1b812e2ec81708f91b649cdab3aa053b5e6b
crp1212 <403454318@qq.com>
2019-09-16 17:08:56 +0800

    feat: antd 配置为中文

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { LocaleProvider } from 'antd'
+import zh_CN from 'antd/es/locale-provider/zh_CN'
 import { HashRouter as Router} from 'react-router-dom'
 import App from '@/App'
 import './assets/common.less'
@@ -9,7 +12,7 @@ import store from './store/index'
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <LocaleProvider locale={zh_CN}><App /></LocaleProvider>
     </Router>  
   </Provider>,
   document.getElementById('root')
